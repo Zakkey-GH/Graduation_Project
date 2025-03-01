7acb324474b2e1a0d6e1c183d34a9ec364928632
Zakkey-GH <reedinortoop@gmail.com>
2025-03-01 12:25:51 +0900

    Next.js から AWS Lambda を呼び出す

---
+++ src/app/api/test.ts
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+    const response = await fetch("https://hq3d4xbkh9.execute-api.us-east-1.amazonaws.com", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: "山崎" }),
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+    }
