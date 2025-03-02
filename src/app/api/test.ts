c56a41c77d9397f76dc598d7cd9e4bf720ea3a94
Zakkey-GH <reedinortoop@gmail.com>
2025-03-02 17:58:25 +0900

    APIエンドポイントの変更

--- src/app/api/test.ts
+++ src/app/api/test.ts
@@ -1,9 +1,15 @@
 export default async function handler(req, res) {
-    const response = await fetch("https://hq3d4xbkh9.execute-api.us-east-1.amazonaws.com", {
+    const response = await fetch("https://hq3d4xbkh9.execute-api.us-east-1.amazonaws.com/testPy", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name: "山崎" }),
     });
+
+    // {
+    //     "method": "POST",
+    //     "headers": { "Content-Type": "application/json" },
+    //     "body": "{\"name\": \"山崎\"}"
+    //   }
 
     const data = await response.json();
     res.status(200).json(data);
